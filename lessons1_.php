09465c9a2a16a99ef3e63809bcbfae3552958b52
Taras Gumeneckij <taras.gumeneckij@gmail.com>
2017-05-19 00:20:25 +0300

    first change

---
+++ lessons1_.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Testing PHP</title>
+ </head>
+ <body>
+ <p>
+ 	 <?php 
+ 	echo "Hello, world!>"  
+ ?>
+ </p>
+
+ </body>
+</html>
+<?php 
+ 	echo "Hello, world!>"  
+ ?>
